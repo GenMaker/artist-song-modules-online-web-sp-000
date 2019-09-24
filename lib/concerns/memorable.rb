5f3d3b72a64290f477dc8cdc97bb7288ecb0deb9
Panela Leung <leungpanela@gmail.com>
2019-09-24 20:59:00 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -11,7 +11,9 @@ module Memorable
 
   module InstanceMethods
    def initialize
-     # some more code coming soon!
+     def initialize
+       self.class.all << self
+     end
    end
- end
+  end
 end
